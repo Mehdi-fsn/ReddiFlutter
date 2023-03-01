78939edf8bebd5303670b6bfbc730cd84a24911f
Mehdi Fessiane <mehdi.fessiane@epitech.eu>
2023-03-01 22:29:31 +0100

    Base of Home Screen => Card RedditPost & filterButton by Type (Best, Hot, New, Rising) | Missing Search Bar

--- redditech/lib/modules/home/home_screen.dart
+++ redditech/lib/modules/home/home_screen.dart
@@ -1,12 +1,205 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class HomeScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:redditech/constants/app_theme.dart';
+import 'package:redditech/models/reddit_post.dart';
+import 'package:redditech/services/api/reddit_api.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  var _type = 'hot';
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Home Page"),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              color: AppTheme.primary,
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: Column(
+              children: [
+                const Center(
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: Center(child: Text('Search Bar')),
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: Expanded(
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _type = 'hot';
+                              });
+                            },
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0),
+                                  side: BorderSide(
+                                    color: (_type == 'hot')
+                                        ? Colors.white
+                                        : Colors.transparent,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            child: Text(
+                              'Hot',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: (_type == 'hot')
+                                    ? Colors.white
+                                    : AppTheme.textColor,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _type = 'new';
+                              });
+                            },
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0),
+                                  side: BorderSide(
+                                    color: (_type == 'new')
+                                        ? Colors.white
+                                        : Colors.transparent,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            child: Text(
+                              'New',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: (_type == 'new')
+                                    ? Colors.white
+                                    : AppTheme.textColor,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _type = 'top';
+                              });
+                            },
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0),
+                                  side: BorderSide(
+                                    color: (_type == 'top')
+                                        ? Colors.white
+                                        : Colors.transparent,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            child: Text(
+                              'Top',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: (_type == 'top')
+                                    ? Colors.white
+                                    : AppTheme.textColor,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _type = 'rising';
+                              });
+                            },
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0),
+                                  side: BorderSide(
+                                    color: (_type == 'rising')
+                                        ? Colors.white
+                                        : Colors.transparent,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            child: Text(
+                              'Rising',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: (_type == 'rising')
+                                    ? Colors.white
+                                    : AppTheme.textColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          FutureBuilder(
+            future: RedditAPI.fetchSubReddit(type: _type),
+            builder: (BuildContext context,
+                AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return const SizedBox(
+                      height: 500,
+                      child: Center(child: CircularProgressIndicator()));
+                default:
+                  return Expanded(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        return RedditPost(
+                          subreddit: snapshot.data![index]["subreddit"],
+                          author: snapshot.data![index]["author"],
+                          title: snapshot.data![index]["title"],
+                          selfText: snapshot.data![index]["selfText"],
+                          thumbnail: snapshot.data![index]["thumbnail"],
+                          score: snapshot.data![index]["score"],
+                          numComments: snapshot.data![index]["numComments"],
+                          createdUtc: snapshot.data![index]["createdUtc"],
+                        );
+                      },
+                    ),
+                  );
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
 }
--- redditech/lib/services/api/reddit_api.dart
+++ redditech/lib/services/api/reddit_api.dart
@@ -9,7 +9,6 @@ import 'package:redditech/utils/format_date.dart';
 import 'package:redditech/services/repositories/user_repository.dart';
 
 abstract class RedditAPI {
-
   static Future<Map<String, dynamic>> fetchRedditUserInfo() async {
     final token = await Modular.get<UserRepository>().getToken();
     final url = Uri.parse('https://oauth.reddit.com/api/v1/me');
@@ -43,11 +42,12 @@ abstract class RedditAPI {
     return res;
   }
 
-  static Future<List<Map<String, dynamic>>> fetchUserPostSubmitted({username}) async {
+  static Future<List<Map<String, dynamic>>> fetchUserPostSubmitted(
+      {username}) async {
     final token = await Modular.get<UserRepository>().getToken();
     username ??= await Modular.get<UserRepository>().getUsername();
     final url =
-    Uri.parse('https://oauth.reddit.com/user/$username/submitted?limit=25');
+        Uri.parse('https://oauth.reddit.com/user/$username/submitted?limit=25');
     final headers = {
       HttpHeaders.authorizationHeader: 'Bearer $token',
       HttpHeaders.userAgentHeader: RedditInfo.userAgent,
@@ -59,17 +59,17 @@ abstract class RedditAPI {
     );
 
     if (response.statusCode != 200) {
-      throw Exception(
-          'Failed to load user posts (/user/$username/submitted)');
+      throw Exception('Failed to load user posts (/user/$username/submitted)');
     }
 
     final jsonBody = jsonDecode(response.body);
     final List<Map<String, dynamic>> posts = [];
-    for(var i = 0; i < jsonBody['data']['dist']; i++) {
-
-      var date = FormatDate.toDateTime(jsonBody['data']['children'][i]['data']['created_utc']);
+    for (var i = 0; i < jsonBody['data']['dist']; i++) {
+      var date = FormatDate.toDateTime(
+          jsonBody['data']['children'][i]['data']['created_utc']);
       Map<String, dynamic> post = {
-        'subreddit': jsonBody['data']['children'][i]['data']['subreddit_name_prefixed'],
+        'subreddit': jsonBody['data']['children'][i]['data']
+            ['subreddit_name_prefixed'],
         'author': 'u/${jsonBody['data']['children'][i]['data']['author']}',
         'title': jsonBody['data']['children'][i]['data']['title'],
         'selfText': jsonBody['data']['children'][i]['data']['selftext'],
@@ -106,14 +106,16 @@ abstract class RedditAPI {
       'lang': jsonBody['lang'],
       'over_18': jsonBody['over_18'],
       'allow_clicktracking': jsonBody['allow_clicktracking'],
-      'show_location_based_recommendations': jsonBody['show_location_based_recommendations'],
+      'show_location_based_recommendations':
+          jsonBody['show_location_based_recommendations'],
     };
 
     print(res);
     return res;
   }
 
-  static Future<bool> updateRedditPreferences(Map<String, dynamic> prefs) async {
+  static Future<bool> updateRedditPreferences(
+      Map<String, dynamic> prefs) async {
     final token = await Modular.get<UserRepository>().getToken();
     final url = Uri.parse('https://oauth.reddit.com/api/v1/me/prefs');
     final headers = {
@@ -134,5 +136,49 @@ abstract class RedditAPI {
 
     return true;
   }
-}
 
+  static Future<List<Map<String, dynamic>>> fetchSubReddit(
+      {required String type, String? subreddit}) async {
+    final token = await Modular.get<UserRepository>().getToken();
+    Uri url;
+    if (subreddit != null) {
+      url = Uri.parse('https://oauth.reddit.com/$subreddit/$type?limit=50');
+    } else {
+      url = Uri.parse('https://oauth.reddit.com/$type?limit=50');
+    }
+    final headers = {
+      HttpHeaders.authorizationHeader: 'Bearer $token',
+      HttpHeaders.userAgentHeader: RedditInfo.userAgent,
+    };
+
+    http.Response response = await http.get(
+      url,
+      headers: headers,
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load user posts (/hot|new|top|best)');
+    }
+
+    final jsonBody = jsonDecode(response.body);
+    final List<Map<String, dynamic>> posts = [];
+    for (var i = 0; i < jsonBody['data']['dist']; i++) {
+      var date = FormatDate.toDateTime(
+          jsonBody['data']['children'][i]['data']['created_utc']);
+      Map<String, dynamic> post = {
+        'subreddit': jsonBody['data']['children'][i]['data']
+            ['subreddit_name_prefixed'],
+        'author': 'u/${jsonBody['data']['children'][i]['data']['author']}',
+        'title': jsonBody['data']['children'][i]['data']['title'],
+        'selfText': jsonBody['data']['children'][i]['data']['selftext'],
+        'thumbnail': jsonBody['data']['children'][i]['data']['thumbnail'],
+        'score': jsonBody['data']['children'][i]['data']['score'],
+        'numComments': jsonBody['data']['children'][i]['data']['num_comments'],
+        'createdUtc': date,
+      };
+      posts.add(post);
+    }
+
+    return posts;
+  }
+}
