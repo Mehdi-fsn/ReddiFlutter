3b6a9b78228b09d9210bf14f7ad0eea6a265bf45
Carmona Maxime <maxime.carmona@epitech.eu>
2023-03-06 16:36:58 +0100

    new screen-home

--- redditech/lib/modules/home/home_screen.dart
+++ redditech/lib/modules/home/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:redditech/constants/app_theme.dart';
 import 'package:redditech/models/reddit_post.dart';
 import 'package:redditech/services/api/reddit_api.dart';
 
@@ -16,155 +15,23 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              color: AppTheme.primary,
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-            ),
-            child: Column(
-              children: [
-                const Center(
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 50,
-                    child: Center(child: Text('Search Bar')),
-                  ),
+    return Scaffold(
+            appBar: AppBar(
+              title: Text('Home'),
+              centerTitle: false,
+              actions: [
+                IconButton(onPressed: () {},
+                  icon: const Icon(Icons.search),
                 ),
-                SizedBox(
-                  width: double.infinity,
-                  child: Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              _type = 'hot';
-                            });
-                          },
-                          style: ButtonStyle(
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: BorderSide(
-                                  color: (_type == 'hot')
-                                      ? Colors.white
-                                      : Colors.transparent,
-                                ),
-                              ),
-                            ),
-                          ),
-                          child: Text(
-                            'Hot',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              color: (_type == 'hot')
-                                  ? Colors.white
-                                  : AppTheme.textColor,
-                            ),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              _type = 'new';
-                            });
-                          },
-                          style: ButtonStyle(
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: BorderSide(
-                                  color: (_type == 'new')
-                                      ? Colors.white
-                                      : Colors.transparent,
-                                ),
-                              ),
-                            ),
-                          ),
-                          child: Text(
-                            'New',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              color: (_type == 'new')
-                                  ? Colors.white
-                                  : AppTheme.textColor,
-                            ),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              _type = 'top';
-                            });
-                          },
-                          style: ButtonStyle(
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: BorderSide(
-                                  color: (_type == 'top')
-                                      ? Colors.white
-                                      : Colors.transparent,
-                                ),
-                              ),
-                            ),
-                          ),
-                          child: Text(
-                            'Top',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              color: (_type == 'top')
-                                  ? Colors.white
-                                  : AppTheme.textColor,
-                            ),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            setState(() {
-                              _type = 'rising';
-                            });
-                          },
-                          style: ButtonStyle(
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: BorderSide(
-                                  color: (_type == 'rising')
-                                      ? Colors.white
-                                      : Colors.transparent,
-                                ),
-                              ),
-                            ),
-                          ),
-                          child: Text(
-                            'Rising',
-                            style: TextStyle(
-                              fontWeight: FontWeight.w700,
-                              color: (_type == 'rising')
-                                  ? Colors.white
-                                  : AppTheme.textColor,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
+                IconButton(
+                  icon:CircleAvatar(
+                    backgroundImage: NetworkImage("https://worldissmall.fr/wp-content/uploads/2021/08/reddit.png"),
                   ),
+                  onPressed: () {},
                 ),
               ],
             ),
-          ),
-          FutureBuilder(
+          body : FutureBuilder(
             future: RedditAPI.fetchSubReddit(type: _type),
             builder: (BuildContext context,
                 AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
@@ -177,8 +44,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   if(snapshot.hasError) {
                     const Center(child:Text('Error loading'));
                   }
-                  return Expanded(
-                    child: ListView.builder(
+                  return  ListView.builder(
                       shrinkWrap: true,
                       itemCount: snapshot.data!.length,
                       itemBuilder: (context, index) {
@@ -193,13 +59,10 @@ class _HomeScreenState extends State<HomeScreen> {
                           createdUtc: snapshot.data![index]["createdUtc"],
                         );
                       },
-                    ),
                   );
               }
             },
           ),
-        ],
-      ),
     );
   }
 }
